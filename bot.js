343c64220a2f114a8072f2395d6cf420f9ba93de
TheTorpian <alexxgabriel18@gmail.com>
2019-02-06 16:49:59 +0100

    added .rape

--- bot.js
+++ bot.js
@@ -1,39 +1,69 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+const prefix = '.';
+
+//idfk what logger is for
+
 // Configure logger settings
-logger.remove(logger.transports.Console);
+// logger.remove(logger.transports.Console);
 // logger.add(logger.transports.Console, {
 //     colorize: true
 // });
-logger.level = 'debug';
+//logger.level = 'debug';
+
 // Initialize Discord Bot
-var bot = new Discord.Client({
+var client = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
+client.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(client.username + ' - (' + client.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    // It will listen for messages that will start with prefix
+    if (message.substring(0, 1) == prefix) {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
-        args = args.splice(1);
+        //args = args.splice(1);
         switch(cmd) {
-            // !ping
+            // .ping
             case 'ping':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
             break;
-            // Just add any case commands if you want to..
-         }
-     }
+
+            // .rape
+            case 'rape':
+                var rapee = args[1];
+                //if args[1] is a user tag
+                if (rapee.substring(0, 2) == '<@'){
+                    client.sendMessage({
+                        to: channelID,
+                        message: rapee + ', ' + '<@' + userID + '>' + ' raped you!'
+                    });
+                }
+                //if tag is vegas bot (doesn't work)
+                else if (rapee == '<@542697185339375616>' || rapee == '@Vegas'){
+                    client.sendMessage({
+                        to: channelID,
+                        message: 'You cannot rape the Vegas Bot.'
+                    });
+                }
+                //not a user tag
+                else {
+                    client.sendMessage({
+                        to: channelID,
+                        message: 'Invalid user.'
+                    });
+                }
+            break;
+        }
+    }
 });
