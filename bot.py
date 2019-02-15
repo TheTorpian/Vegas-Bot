582bf876dcc2b144831b7e91d1cd0d4de355a5c1
TheTorpian <alexxgabriel18@gmail.com>
2019-02-15 23:11:30 +0100

    Update bot.py
    Added more responses, removed spaces from args, added test command to print all args

--- bot.py
+++ bot.py
@@ -13,11 +13,16 @@ client = Bot(command_prefix=BOT_PREFIX)
 @client.event
 async def on_message(message):
     if message.content.startswith('.'): #actual working prefix
-        if message.author.bot: return # blocks bots from using commands
+        if message.author.bot: return #blocks bots from using commands
 
-        usrTag = 0
         cmd = message.content.split()[0].lower()[1:] #get the command from user message
         args = message.content.split(' ')[1:] #split the rest of the message
+
+        for argCounter in range(0, len(args)):
+            try:
+                args.remove('')
+            except Exception:
+                pass
 
         #rape command
         if cmd == 'rape':
@@ -36,26 +41,47 @@ async def on_message(message):
             'you went to the future, to come back in the present to fuck yourself.'
             ]
             #self rape
-            if args[usrTag].replace('!', '') == message.author.mention.replace('!', '') or args[usrTag] == message.author.mention.replace('!', ''):
+            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
                 await client.send_message(message.channel, random.choice(possible_responses_self))
             #Vegas Bot rape
-            elif args[usrTag].replace('!', '') == '<@542697185339375616>':
+            elif args[0].replace('!', '') == '<@542697185339375616>':
                 await client.send_message(message.channel, "You can't rape the Vegas Bot.")
             #other bots rape
-            elif args[usrTag].replace('!', '') == '<@367835200916291586>' or args[usrTag].replace('!', '') == '<@389937555853934593>':
-                await client.send_message(message.channel, "Not a good idea to fuck a bot.")
+            elif args[0].replace('!', '') == '<@367835200916291586>' or args[0].replace('!', '') == '<@389937555853934593>':
+                await client.send_message(message.channel, 'Not a good idea to fuck a bot.')
             #all other rapes
             else:
                 await client.send_message(message.channel, message.author.mention + random.choice(possible_responses) + ' '.join(args))
 
         if cmd == 'molest':
-            await client.send_message(message.channel, message.author.mention + ' molested ' + ' '.join(args))
+            #tag self
+            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+                await client.send_message(message.channel, message.author.mention + ' tried to molest themselves.')
+            #tag vegas bot
+            elif args[0].replace('!', '') == '<@542697185339375616>':
+                await client.send_message(message.channel, 'You cannot molest the Vegas Bot.')
+            else:
+                await client.send_message(message.channel, message.author.mention + ' molested ' + ' '.join(args))
 
         if cmd == 'touch':
-            await client.send_message(message.channel, message.author.mention + ' touched ' + ' '.join(args))
+            #tag self
+            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+                await client.send_message(message.channel, message.author.mention + ' touched themselves reaaaaaal good.')
+            #tag vegas bot
+            elif args[0].replace('!', '') == '<@542697185339375616>':
+                await client.send_message(message.channel, 'You may touch the Vegas Bot.')
+            else:
+                await client.send_message(message.channel, message.author.mention + ' touched ' + ' '.join(args))
 
         if cmd == 'fill':
-            await client.send_message(message.channel, message.author.mention + ' filled ' + ' '.join(args) + ' all the way up ;))')
+            #tag self
+            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+                await client.send_message(message.channel, message.author.mention + ' filled... themselves up?')
+            #tag vegas bot
+            elif args[0].replace('!', '') == '<@542697185339375616>':
+                await client.send_message(message.channel, 'You cannot fill the Vegas Bot.')
+            else:
+                await client.send_message(message.channel, message.author.mention + ' filled ' + ' '.join(args) + ' all the way up ;))')
 
         #rape for 2 tags
         if cmd == 'gangrape':
@@ -69,7 +95,6 @@ async def on_message(message):
             ' priested ',
             ' step dadded '
             ]
-            argCounter = 1
             msg = message.author.mention + random.choice(possible_responses) + args[0]
             for argCounter in range(1, len(args)):
                 msg += ' and ' + args[argCounter]
@@ -86,6 +111,12 @@ async def on_message(message):
             'https://tenor.com/3oVt.gif'
             ]
             await client.send_message(message.channel, random.choice(possible_ricardos))
+
+        #test command to print all args
+        if cmd == 'getargs':
+            for argCounter in range(0, len(args)):
+                await client.send_message(message.channel, '`' + args[argCounter] + '`')
+
 
 @client.event
 async def on_ready():
