d8636e5716e8b9c14ccf3ec72de54c14135558ac
TheTorpian <alexxgabriel18@gmail.com>
2019-02-15 23:16:06 +0100

    Update bot.py
    commented out loop function to print active servers

--- bot.py
+++ bot.py
@@ -122,15 +122,18 @@ async def on_message(message):
 async def on_ready():
     await client.change_presence(game=Game(name="blackjack"))
     print("Logged in as " + client.user.name)
+    print("Current servers:")
+    for server in client.servers:
+        print(server.name)
 
-async def list_servers():
-    await client.wait_until_ready()
-    while not client.is_closed:
-        print("Current servers:")
-        for server in client.servers:
-            print(server.name)
-        await asyncio.sleep(600)
+# async def list_servers():
+#     await client.wait_until_ready()
+#     while not client.is_closed:
+#         print("Current servers:")
+#         for server in client.servers:
+#             print(server.name)
+#         await asyncio.sleep(600)
 
 
-client.loop.create_task(list_servers())
+# client.loop.create_task(list_servers())
 client.run(TOKEN)
