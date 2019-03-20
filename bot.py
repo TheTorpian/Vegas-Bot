f57bda7f16e21bc3adc9f091494c04ac2b3d78c2
TheTorpian <alexxgabriel18@gmail.com>
2019-03-20 19:09:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from discord.ext.commands import Bot
 
 BOT_PREFIX = ('.', '$') # currently useless
 TOKEN = 'NTQyNjk3MTg1MzM5Mzc1NjE2.Dz-SCg._sflidalhgN-_Upl6W9OPX1w1o4' # Get at discordapp.com/developers/applications/me
+INVITE = 'https://discordapp.com/api/oauth2/authorize?client_id=542697185339375616&permissions=379968&scope=bot' # bot invite link
 
 client = Bot(command_prefix=BOT_PREFIX)
 
@@ -18,6 +19,15 @@ async def on_message(message):
 
         cmd = message.content.split()[0].lower()[1:] # get the command from user message
         args = message.content.split(' ')[1:] # split the rest of the message
+
+        authorTag = message.author.mention.replace('!', '')
+        vegasBotTag = '<@542697185339375616>'
+        tag = ' '
+        try:
+            if args[0][:1] == '<': # if first arg is a tag, make a variable
+                tag = args[0].replace('!', '')
+        except Exception:
+            pass
 
         for argCounter in range(0, len(args)):
             try:
@@ -42,54 +52,51 @@ async def on_message(message):
             'you went to the future, to come back in the present to fuck yourself.'
             ]
             # self rape
-            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+            if tag == authorTag:
                 await client.send_message(message.channel, random.choice(possible_responses_self))
             # Vegas Bot rape
-            elif args[0].replace('!', '') == '<@542697185339375616>':
+            elif tag == vegasBotTag:
                 await client.send_message(message.channel, "You can't rape the Vegas Bot.")
-            # other bots rape
-            elif args[0].replace('!', '') == '<@367835200916291586>' or args[0].replace('!', '') == '<@389937555853934593>':
-                await client.send_message(message.channel, 'Not a good idea to fuck a bot.')
             # all other rapes
             else:
                 await client.send_message(message.channel, message.author.mention + random.choice(possible_responses) + ' '.join(args))
 
         if cmd == 'molest':
             # tag self
-            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+            if tag == authorTag:
                 await client.send_message(message.channel, message.author.mention + ' tried to molest themselves.')
             # tag vegas bot
-            elif args[0].replace('!', '') == '<@542697185339375616>':
+            elif tag == vegasBotTag:
                 await client.send_message(message.channel, 'You cannot molest the Vegas Bot.')
             else:
                 await client.send_message(message.channel, message.author.mention + ' molested ' + ' '.join(args))
 
         if cmd == 'touch':
             # tag self
-            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+            if tag == authorTag:
                 await client.send_message(message.channel, message.author.mention + ' touched themselves reaaaaaal good.')
             # tag vegas bot
-            elif args[0].replace('!', '') == '<@542697185339375616>':
+            elif tag == vegasBotTag:
                 await client.send_message(message.channel, 'You may touch the Vegas Bot.')
             else:
                 await client.send_message(message.channel, message.author.mention + ' touched ' + ' '.join(args))
 
         if cmd == 'fill':
             # tag self
-            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+            if tag == authorTag:
                 await client.send_message(message.channel, message.author.mention + ' filled... themselves up?')
             # tag vegas bot
-            elif args[0].replace('!', '') == '<@542697185339375616>':
+            elif tag == vegasBotTag:
                 await client.send_message(message.channel, 'You cannot fill the Vegas Bot.')
             else:
                 await client.send_message(message.channel, message.author.mention + ' filled ' + ' '.join(args) + ' all the way up ;))')
 
         if cmd == 'succ':
             # tag self
-            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+            if tag == authorTag:
                 await client.send_message(message.channel, message.author.mention + ' got their own succ')
             # tag vegas bot
-            elif args[0].replace('!', '') == '<@542697185339375616>':
+            elif tag == vegasBotTag:
                 await client.send_message(message.channel, 'The Vegas Bot gave you the succ.')
             else:
                 await client.send_message(message.channel, message.author.mention + ' gave ' + ' '.join(args) + ' dat good succ')
@@ -124,7 +131,7 @@ async def on_message(message):
             'https://tenor.com/3oVt.gif',
             'https://imgur.com/a/pM5R4UM',
             ]
-            # if message.author.mention.replace('!', '') == '<@162966505430974464>':
+            # if authorTag == '<@162966505430974464>':
             #     await client.send_message(message.channel, 'bad Arassii')
             # else:
             await client.send_message(message.channel, random.choice(possible_ricardos))
@@ -136,14 +143,15 @@ async def on_message(message):
         # bitchslaps the tagged user
         if cmd == 'bitchslap':
             #tag self
-            if args[0].replace('!', '') == message.author.mention.replace('!', '') or args[0] == message.author.mention.replace('!', ''):
+            if tag == authorTag:
                 await client.send_message(message.channel, message.author.mention + ' bitchslapped themselves')
             #tag vegas bot
-            elif args[0].replace('!', '') == '<@542697185339375616>':
+            elif tag == vegasBotTag:
                 await client.send_message(message.channel, 'You cannot bitchslap the Vegas Bot.')
             else:
                 msg = message.author.mention + ' bitchslapped ' + ' '.join(args)
                 await client.send_message(message.channel, msg)
+
         # fuck off meme
         if cmd == 'fuckoff':
             await client.send_message(message.channel, 'https://imgur.com/a/RMXA4xP')
@@ -158,31 +166,28 @@ async def on_message(message):
 
             outcome = random.choice(possible_outcomes)
 
-            tagger = message.author.mention.replace('!', '')
-            tagged = args[0].replace('!', '')
-
-            if tagged == '<@542697185339375616>':
+            if tag == vegasBotTag:
                 await client.send_message(message.channel, "You always lose against Vegas Bot.")
             else:
                 if outcome == 'tagged':
-                    if tagged == tagger:
+                    if tag == authorTag:
                         await client.send_message(message.channel, 'You lost against... yourself?')
                     else:
-                        await client.send_message(message.channel, tagged + ' won!')
+                        await client.send_message(message.channel, tag + ' won!')
                 if outcome == 'tagger':
-                    if tagged == tagger:
+                    if tag == authorTag:
                         await client.send_message(message.channel, 'You won! But also lost...?')
                     else:
-                        await client.send_message(message.channel, tagger + ' won!')
+                        await client.send_message(message.channel, message.author.mention + ' won!')
                 if outcome == 'no one':
-                    if tagged == tagger:
-                        await client.send_message(message.channel, 'You lost twice!')
+                    if tag == authorTag:
+                        await client.send_message(message.channel, 'You REALLY lost against yourself')
                     else:
                         await client.send_message(message.channel, 'You both lost!')
 
         # invite link
         if cmd == 'invite':
-            await client.send_message(message.channel, 'https://discordapp.com/api/oauth2/authorize?client_id=542697185339375616&permissions=379968&scope=bot')
+            await client.send_message(message.channel, INVITE)
 
         # debug command to print all message args
         if cmd == 'getargs':
