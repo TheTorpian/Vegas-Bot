a7b813938a491e2688a6c69b7f86fb137ba72321
TheTorpian <alexxgabriel18@gmail.com>
2019-06-13 15:15:29 +0200

    reworked ricardo command

--- cogs/ricardo.py
+++ cogs/ricardo.py
@@ -1,8 +1,10 @@
 import random
+import requests
 from discord.ext import commands
 from tokenfile import Vars
 
 bigric = Vars.bigric
+apikey = Vars.apikey
 
 
 class RicardoCog(commands.Cog):
@@ -11,18 +13,24 @@ class RicardoCog(commands.Cog):
 
     @commands.command()  # random ricardo gif or meme
     async def ricardo(self, ctx):
-        possible_ricardos = [
-            'https://tenor.com/2Aql.gif',
-            'https://tenor.com/4f6F.gif',
-            'https://tenor.com/3KGw.gif',
-            'https://tenor.com/3WCg.gif',
-            'https://tenor.com/3oVt.gif',
-            'https://imgur.com/a/pM5R4UM',
-            'https://imgur.com/a/3qtdO1e',
-            '<a:ricardoBear:564806311791820810>',
-            bigric
-        ]
-        await ctx.send(random.choice(possible_ricardos))
+        search = f'https://api.tenor.com/v1/search?q=ricardo&key={apikey}'  # searches tenor for ricardo with my apikey
+        get = requests.get(search)
+        if get.status_code == 200:  # get successful
+            json_search = get.json()
+            json_check = json_search['next']  # number of results, is string
+            if json_check == '0':
+                await ctx.send('No gifs found.')
+            else:
+                rand_gif = random.randint(1, int(json_check))  # random gif from those found
+                json_s = json_search['results']  # get the results
+                gif = json_s[rand_gif]  # get the random gif
+                gif = gif.get('media')  # go into media, various file types etc
+                gif = gif[0]  # has to get the first element
+                gif = gif.get('gif')  # get the gif element
+                gif = gif.get('url')  # get url of gif
+                await ctx.send(gif)
+        elif get.status_code == 404:
+            await ctx.send('Error 404!')
 
     @commands.command()  # ricardo bear
     async def ricardobear(self, ctx):
