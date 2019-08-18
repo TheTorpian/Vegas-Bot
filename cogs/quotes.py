de13db0f3091c81587e007cce950ff2299a803dc
TheTorpian <alexxgabriel18@gmail.com>
2019-08-18 16:17:59 +0200

    updated quote system, added quote_list

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -1,4 +1,5 @@
 import re
+import random
 from discord.ext import commands
 from sql import sql_quotes
 
@@ -9,17 +10,24 @@ class QuotesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def quote(self, ctx):
+    @commands.command()  # gets a random or selected quote from server
+    async def quote(self, ctx, quote_nr = 0):
         try:
-            quote = sql_quotes.rand_quote(ctx.guild.id)
+            quotes = sql_quotes.get_quote(ctx.guild.id)
+            if quote_nr == 0:  # if arg is 0 or is command is called without args
+                quote_nr = random.randint(0, len(quotes) - 1)
+            else:
+                quote_nr -= 1
+            quote = quotes[quote_nr]
             time = quote[3].strftime(tformat)
-            str_quote = f'Quote #{quote[0]}: {quote[2]} - {quote[1]} ({time})'
+            await ctx.send(quote_nr)
+            str_quote = f'Quote #{quote_nr+1}: {quote[2]} - {quote[1]} ({time})'
             await ctx.send(str_quote)
         except IndexError:
+            await ctx.send(quote_nr)
             await ctx.send('No quote found')
 
-    @commands.command()
+    @commands.command()  # adds quote for server
     async def add_quote(self, ctx):
         author = re.search(r'(\s[^-"]*$)', ctx.message.content)
         quote = re.search(r'"[^"]*"', ctx.message.content)
@@ -27,7 +35,20 @@ class QuotesCog(commands.Cog):
             sql_quotes.add_quote(author.group(0)[1:], quote.group(0), ctx.guild.id)
             await ctx.send('Quote added')
         else:
-            await ctx.send('Wrong format')
+            await ctx.send('Wrong format, do `"quote" - user`')
+
+    @commands.command()  # gets all the quotes from the server
+    async def quote_list(self, ctx):
+        try:
+            quotes = sql_quotes.get_quote(ctx.guild.id)
+            str_quotes = ''
+            for idx, quote in enumerate(quotes, start=1):
+                time = quote[3].strftime(tformat)
+                str_quote = f'Quote #{idx}: {quote[2]} - {quote[1]} ({time})'
+                str_quotes += f'{str_quote}\n'
+            await ctx.send(str_quotes)
+        except IndexError:
+            await ctx.send('No quotes found')
 
 
 def setup(bot):
--- sql/sql_quotes.py
+++ sql/sql_quotes.py
@@ -1,4 +1,3 @@
-import random
 import tokenfile
 from tokenfile import connection
 
@@ -12,9 +11,9 @@ def add_quote(tag, quote, sv):  # adds quote to db
     db.commit()
 
 
-def rand_quote(sv):  # gets a random quote from current server
+def get_quote(sv):  # gets a random quote from current server
     cursor = tokenfile.get_cursor(connection)
     query = 'SELECT quote_id, quoted_tag, quote, tstamp FROM Quotes WHERE server_id_e=%s'
     cursor.execute(query, (sv,))
     quotes = cursor.fetchall()  # returns list of tuples, use double index to get actual values
-    return random.choice(quotes)
+    return quotes
